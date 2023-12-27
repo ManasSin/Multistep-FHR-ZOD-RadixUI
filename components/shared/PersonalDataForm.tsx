8d1f87a2cf4706ca557f42565b50fe1db6924745
ManasSin <maltisadan12@gmail.com>
2023-12-27 16:28:26 +0530

    FAILED: ignore

---
+++ components/shared/PersonalDataForm.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TPersonalDetailsScehma } from "@/lib/types";
+import { personalDataSchema } from "@/schema/zodSchema";
+
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+type Props = {};
+
+function PersonalDataForm({}: Props) {
+  const form = useForm<TPersonalDetailsScehma>({
+    resolver: zodResolver(personalDataSchema.partial()),
+    defaultValues: {
+      address: "",
+      age: 18,
+      city: "",
+      email: "",
+      fristName: "",
+      lastName: "",
+      zipcode: "PP00000",
+    },
+  });
+
+  // console.log(personalDataSchema._cached.keys);
+
+  const handleSubmit = (value: TPersonalDetailsScehma) => {
+    // e.preventDefault();
+
+    console.log(value);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="flex flex-col gap-y-5"
+      >
+        <FormField
+          control={form.control}
+          name="fristName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>FristName</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter FristName" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Lastname</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Lastname" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="age"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Age</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Age"
+                  {...field}
+                  type="number"
+                  className="w-fit block"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+export default PersonalDataForm;
