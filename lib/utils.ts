2df8f79a476c923ac9ee8276ea06c1fc1ba4f97b
ManasSin <maltisadan12@gmail.com>
2023-12-27 23:22:10 +0530

    feat:
    added types for future use and easy access

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const delay = (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time));
