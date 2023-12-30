8b066065d85032c29881575d59999ff9d948a785
ManasSin <maltisadan12@gmail.com>
2023-12-30 18:56:15 +0530

    CHORE: changed variable name to add more sense to it

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,6 @@
 import { AuthSchema, personalDataSchema } from "@/schema/zodSchema";
 import * as z from "zod";
 
-export type TSignUpSchema = z.infer<typeof AuthSchema>;
+export type TSignInSchema = z.infer<typeof AuthSchema>;
 export type TPersonalDetailsScehma = z.infer<typeof personalDataSchema>;
 export type FieldValues = keyof TPersonalDetailsScehma;
