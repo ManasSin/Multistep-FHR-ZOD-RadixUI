4983d5b954fd90e0cef171b8daa3791b5a9abf2e
ManasSin <maltisadan12@gmail.com>
2024-01-02 20:05:43 +0530

    REFACTOR: changed main page to show form at front page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Form />
+    <main className="flex min-h-fit flex-col items-center justify-between p-[5%] max-w-xl md:max-w-2xl lg:max-w-5xl w-full mx-auto">
+      <div className="container">
+        <Form />
+      </div>
     </main>
   );
 }
